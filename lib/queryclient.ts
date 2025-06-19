72bc2db9be890c01b00cab95a5e008fcd5d60934
Md Hasib Sikder <mdhasdibsolder@gmail.com>
2025-06-20 01:57:10 +0600

    Create queryclient.ts

---
+++ lib/queryclient.ts
@@ -0,0 +1,13 @@
+// lib/queryClient.ts
+'use client'
+
+import { QueryClient } from '@tanstack/react-query'
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  }
+})
